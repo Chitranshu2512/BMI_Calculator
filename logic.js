612312c4082cc1e8372c586ed1dac179b338af9c
Chitranshu Kushwaha <118387802+Chitranshu2512@users.noreply.github.com>
2024-01-23 15:01:54 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit' , function(e){
+  e.preventDefault();
+  const height = parseInt(document.querySelector('#height').value)
+  const weight = parseInt(document.querySelector('#weight').value)
+  const result = document.querySelector('#results')
+
+  if(height === '' || height < 0 || isNaN(height)){
+    result.innerHTML = `enter a valid height ${height}`
+  }else if(weight === '' || weight < 0 || isNaN(weight)){
+    result.innerHTML = `enter a valid weight ${weight}`
+  }else{
+    const bmi = (weight / ((height * height)/10000)).toFixed(2)
+    let ans ;
+    if(bmi < 18.6){
+        ans = 'under Weight'
+    }else if(bmi >= 18.6 && bmi < 24.9){
+        ans = 'normal range'
+    }else{
+        ans = 'Over Weight'
+    }
+
+    result.innerHTML = `<span>${bmi}</span> ${ans}`;
+  }
+})
